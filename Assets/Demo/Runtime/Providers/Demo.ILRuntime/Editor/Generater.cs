d2d03e4bdb11ffd45146c7c100f1165c8cc69e44
喵喵大人 <support@catlib.io>
2019-03-19 16:49:08 +0800

    resolved #3

--- Assets/Demo/Runtime/Providers/Demo.ILRuntime/Editor/Generater.cs
+++ Assets/Demo/Runtime/Providers/Demo.ILRuntime/Editor/Generater.cs
@@ -12,6 +12,7 @@
 using ILRuntime.Runtime.CLRBinding;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Demo.API.Debug;
 using Demo.API.HotfixEntry;
 using UnityEditor;
@@ -22,7 +23,7 @@ namespace CatLib.ILRuntime
     /// <summary>
     /// 代码生成器
     /// </summary>
-    public class Generater : MonoBehaviour
+    public class Generater : ScriptableObject
     {
         [MenuItem("CatLib/Generater/Generate CLR Binding Code")]
         public static void GenerateCLRBinding()
@@ -33,7 +34,10 @@ namespace CatLib.ILRuntime
                 typeof(IDebug)
             };
 
-            BindingCodeGenerator.GenerateBindingCode(types, "Assets/Demo/Runtime/Providers/Demo.ILRuntime/Generated/CLR");
+            var ms = MonoScript.FromScriptableObject(CreateInstance(typeof(Generater)));
+            var path = Path.GetDirectoryName(AssetDatabase.GetAssetPath(ms));
+            path = Path.Combine(path, ".." + Path.DirectorySeparatorChar);
+            BindingCodeGenerator.GenerateBindingCode(types, Path.Combine(path, "Generated/CLR"));
         }
     }
 }
